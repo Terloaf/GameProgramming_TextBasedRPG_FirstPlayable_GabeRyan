e134103cc05d5c26e19c88b0729a6fc122a91261
Terloaf <greenyryan2501@gmail.com>
2025-11-17 10:41:24 -0400

    lotta stuff

--- GameProgramming_TextBasedRPG_FirstPlayable_GabeRyan/Program.cs
+++ GameProgramming_TextBasedRPG_FirstPlayable_GabeRyan/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -21,22 +22,29 @@ namespace GameProgramming_TextBasedRPG_FirstPlayable_GabeRyan
 
         static bool Playing = true;
 
+        static string[] map;
+        
 
-        static char[,] map = new char[,]
-        {
-            {'`','`','`','`','`','`','`','`','`','~','~','`','`','`','`','`','`','`','`','`','`','┌','─','─','┐', },
-            {'`','`','`','`','`','`','`','`','~','~','`','`','`','`','`','`','`','`','`','`','`','│','`','`','│', },
-            {'`','`','`','`','`','`','`','~','~','`','`','`','`','`','`','`','`','`','`','`','`','│','`','`','│', },
-            {'`','`','`','~','~','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','└','`','`','┘', },
-            {'~','`','~','~','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`', },
-            {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`', },
-            {'`','`','`','`','`','`','`','`','`','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`','`', },
-            {'`','`','`','`','`','`','`','`','`','^','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`', },
-            {'`','`','`','`','`','`','`','`','`','`','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`', },
-            {'`','`','`','`','`','`','`','`','`','`','`','^','`','`','`','`','`','`','`','`','`','`','`','`','`', },
-            {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`', },
-            {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`', },
-        };
+
+        //static char[,] map = new char[,]
+        //{
+        //    {'`','`','`','`','`','`','`','`','`','~','~','`','`','`','`','`','`','`','`','`','`','┌','─','─','┐', },
+        //    {'`','`','`','`','`','`','`','`','~','~','`','`','`','`','`','`','`','`','`','`','`','│','`','`','│', },
+        //    {'`','`','`','`','`','`','`','~','~','`','`','`','`','`','`','`','`','`','`','`','`','│','`','`','│', },
+        //    {'`','`','`','~','~','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','└','`','`','┘', },
+        //    {'~','`','~','~','~','~','~','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`', },
+        //    {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`', },
+        //    {'`','`','`','`','`','`','`','`','`','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`','`', },
+        //    {'`','`','`','`','`','`','`','`','`','^','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`', },
+        //    {'`','`','`','`','`','`','`','`','`','`','^','^','`','`','`','`','`','`','`','`','`','`','`','`','`', },
+        //    {'`','`','`','`','`','`','`','`','`','`','`','^','`','`','`','`','`','`','`','`','`','`','`','`','`', },
+        //    {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`', },
+        //    {'`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`','`', },
+        //};
+
+        static string mapData = "mapData.txt";
+
+        static string[] boarder = { "═", "║" };
 
         static void Main(string[] args)
         {
@@ -49,25 +57,35 @@ namespace GameProgramming_TextBasedRPG_FirstPlayable_GabeRyan
                 PlayerHandler();
                 PlayerDraw();
                 DisplayMap();
-                Thread.Sleep(25);
                 
+                
+                Thread.Sleep(17);
+
             }
             
         }
-
         static void DisplayMap()
         {
-            for (int mapRow = 0; mapRow < map.GetLength(0); mapRow++)
+            if (File.Exists("mapData.txt"))
             {
-                for(int mapColumn = 0; mapColumn < map.GetLength(1); mapColumn++)
-                {
-                    Console.SetCursorPosition(mapColumn, mapRow);
-                    Console.Write(map[mapRow,mapColumn]);
-                    
-                }
+                map = File.ReadAllLines("mapData.txt");
+
+                Console.WriteLine(map);
             }
-            
         }
+        //static void DisplayMap()
+        //{
+        //    for (int mapRow = 0; mapRow < map.GetLength(0); mapRow++)
+        //    {
+        //        for(int mapColumn = 0; mapColumn < map.GetLength(1); mapColumn++)
+        //        {
+        //            Console.SetCursorPosition(mapColumn, mapRow);
+        //            Console.Write(map[mapRow,mapColumn]);
+                    
+        //        }
+        //    }
+            
+        //}
 
         static void PlayerHandler()
         {
@@ -91,45 +109,51 @@ namespace GameProgramming_TextBasedRPG_FirstPlayable_GabeRyan
 
             // Checks if player is trying to go into water or trees
 
-            if (playerYinput == -1 && map[playerYpos, playerXpos] == '~')
+            if (playerYinput == -1 && map[playerYpos][playerXpos] == '~')
             {
                 playerYpos += 1;
                 
 
 
             }
-            else if (playerYinput == 1 && map[playerYpos, playerXpos] == '~')
+            else if (playerYinput == 1 && map[playerYpos][playerXpos] == '~')
             {
                 playerYpos -= 1;
             }
-            else if (playerXinput == -1 && map[playerYpos, playerXpos] == '~')
+            else if (playerXinput == -1 && map[playerYpos][playerXpos] == '~')
             {
                 playerXpos += 1;
             }
-            else if (playerXinput == 1 && map[playerYpos, playerXpos] == '~')
+            else if (playerXinput == 1 && map[playerYpos][playerXpos] == '~')
             {
                 playerXpos -= 1;
             }
 
-            if (playerYinput == -1 && map[playerYpos, playerXpos] == '^')
+            else if (playerYinput == -1 && map[playerYpos][playerXpos] == '^')
             {
                 playerYpos += 1;
 
 
 
             }
-            else if (playerYinput == 1 && map[playerYpos, playerXpos] == '^')
+            else if (playerYinput == 1 && map[playerYpos][playerXpos] == '^')
             {
                 playerYpos -= 1;
             }
-            else if (playerXinput == -1 && map[playerYpos, playerXpos] == '^')
+            else if (playerXinput == -1 && map[playerYpos][playerXpos] == '^')
             {
                 playerXpos += 1;
             }
-            else if (playerXinput == 1 && map[playerYpos, playerXpos] == '^')
+            else if (playerXinput == 1 && map[playerYpos][playerXpos] == '^')
             {
                 playerXpos -= 1;
             }
+
+
+
+            // Checks border of the map
+
+            
 
         }
 
@@ -138,6 +162,8 @@ namespace GameProgramming_TextBasedRPG_FirstPlayable_GabeRyan
             
             Console.SetCursorPosition(playerXpos, playerYpos);
             Console.Write("O");
+
+            Console.SetCursorPosition(0, 0);
         }
     }
 }
